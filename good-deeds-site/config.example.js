d88f17c36c3e714c44c865bc9b4683d85fe23d78
1 <yurapon11972020@gmail.com>
2026-04-15 18:39:57 +0500

    Delete good-deeds-site directory

--- good-deeds-site/config.example.js
+++
@@ -1,2 +0,0 @@
-export const SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co';
-export const SUPABASE_ANON_KEY = 'YOUR_PUBLIC_ANON_KEY';
--- good-deeds-site/config.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = 'https://dyzgxdalskaqbdmdfmve.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_YYMMtPy1oWakk36i7P4mxg_LxzgSATc';
-export const ADMIN_EMAIL = 'skibidi@mail.ru';
